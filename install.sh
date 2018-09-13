56ab56af90958d385d80529c3ee9bf43961a12e3
James Swineson <github@public.swineson.me>
2018-09-13 21:36:12 +0800

    experimental test script

---
+++ install.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+if [[ $EUID -ne 0 ]]; then
+   echo "This script must be run as root"
+   exit 1
+fi
+
+pushd src
+make
+chmod +x dmswitch
+mkdir -p /usr/local/bin
+mv dmswitch /usr/local/bin
+popd
+
+pushd config
+mkdir -p /etc/dmswitch
+cp config.toml /etc/dmswitch
+cp dmswitch.service /etc/systemd/system
+popd config
